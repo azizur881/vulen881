04d693c721d13c605fbcebacd53564728b0a2c23
azizur881 <azizur881@gmail.com>
2020-10-06 20:34:31 +0600

    done

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -1,12 +1,26 @@
 import React, { useContext } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { UserContext } from '../../App';
-import data from '../../fakeData/users'
-
+import Data from '../../fakeData/users'
 const Item = () => {
+    const select = Data.slice(0,1)
+    const [selected ,setSelected] = useState(select)
+
         return (
-        <div>
-        <h1>Total Item You Have Selected</h1>
+            <div>
+       <h1>You have selected {selected.length}</h1>
+            <ul>
+                {
+                    selected.map(item =>
+                         <img style={{width:"100px",height:"auto",margin:"40px"}}src={item.img} alt=""/> 
+                    )
+                }
+                {
+                    selected.map(item =>
+                    <h3>{item.name}</h3> )
+                }
+            </ul>
         </div>
     );
 };
